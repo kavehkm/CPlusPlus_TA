ab5061a2865bac142018a8aec14e81bfedac0406
kavehkm <mehrbaniankaveh@gmail.com>
2021-12-09 14:16:04 -0500

    add new exercises: 1) account balance 2) sizeof function

---
+++ 01/5_account_balance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int transaction;
+    int balance = 0;
+
+    cout << "enter value of first transaction: ";
+    cin >> transaction;
+    balance += transaction;
+
+    cout << "enter value of second transaction: ";
+    cin >> transaction;
+    balance += transaction;
+
+    cout << "enter value of third transaction: ";
+    cin >> transaction;
+    balance += transaction;
+
+    cout << "balance is: " << balance;
+}
---
+++ 01/6_sizeof.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "size of `char` is: " << sizeof(char) << " byte" << endl;
+    cout << "size of `short int` is: " << sizeof(short int) << " bytes" << endl;
+    cout << "size of `int` is: " << sizeof(int) << " bytes" << endl;
+    cout << "size of `long int` is: " << sizeof(long int) << " bytes" << endl;
+}
