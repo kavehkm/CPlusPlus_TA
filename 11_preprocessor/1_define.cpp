09f92c52bcd19df1c3f40af185067e4c3f571359
kavehkm <mehrbaniankaveh@gmail.com>
2022-02-22 14:25:45 -0500

    create chapter 11: preprocessors: 1) introduce #define

---
+++ 11_preprocessor/1_define.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+// numeric
+#define PI 3.1415
+
+// string
+#define NAME "kaveh"
+
+
+int main () {
+    cout << "pi = " << PI << endl;
+    cout << "name = " << NAME << endl;
+    return 0;
+}
