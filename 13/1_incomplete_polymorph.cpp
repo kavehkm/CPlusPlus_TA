99da0f1b4f26953c1e7d74b5850e25b9126c8575
kavehkm <mehrbaniankaveh@gmail.com>
2022-03-10 15:30:48 -0500

    start chapter 13: polymorphism 1) also define problem of incomplete polymorphism

---
+++ 13/1_incomplete_polymorph.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Base
+{
+    public:
+        void print () const;
+};
+
+void Base :: print () const {
+    cout << "In the Base" << endl;
+}
+
+
+class Derived : public Base
+{
+    public:
+        void print () const;
+};
+
+void Derived :: print () const {
+    cout << "In the Derived" << endl;
+}
+
+
+int main () {
+    Base* ptr;
+
+    ptr = new Base();
+    ptr->print();
+
+    delete ptr;
+
+    ptr = new Derived();
+    ptr->print();
+    
+    delete ptr;
+
+    return 0;
+}
